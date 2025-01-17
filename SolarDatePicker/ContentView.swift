452cb8792e82960c666ed791515e8ab7e34a19ad
Hessam Mahdiabadi <h.mahdiabadi@bluteam.ir>
2025-01-17 20:01:36 +0330

    Initial Commit

---
+++ SolarDatePicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SolarDatePicker
+//
+//  Created by HEssam on 1/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SolarDatePicker/SolarDatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarDatePickerApp.swift
+//  SolarDatePicker
+//
+//  Created by HEssam on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SolarDatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
